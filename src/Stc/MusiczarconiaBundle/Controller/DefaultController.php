df4f60acab9c62de90980271bac97e3a1dd202a4
Jesse Griffin <hemham914@gmail.com>
2014-12-19 13:38:41 -0800

    Fixed major design flaw in converting database time to the id fields of the select boxes.
    Retested the timeslot system, made minor modifications.

--- src/Stc/MusiczarconiaBundle/Controller/DefaultController.php
+++ src/Stc/MusiczarconiaBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Stc\MusiczarconiaBundle\Controller;
 use Stc\MusiczarconiaBundle\Tools\DateTimeTools;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
 {
@@ -26,45 +27,17 @@ class DefaultController extends Controller
     {
         $request = $this->get('request');
         $date = $request->request->get('dateclick');
-        $dateObj = new \DateTime($date);
-        $date = date_format($dateObj, 'Y-m-d');
+
+        //$dateObj = new \DateTime($date);
+        //$date = date_format($dateObj, 'Y-m-d');
         //print_r($request->request);exit;
         $schedulerRepository = $this->get('stc_musiczarconia.repository.scheduler');
 
         $todaysReservations = $schedulerRepository->findReservationByDate($date);
         //print_r($todaysReservations);exit;
-        $htmlClasses = array();
+        //$htmlClasses = array();
 
-        foreach ($todaysReservations as $res) {
-            $stime = $res['reservation_time'];
-            $duration = $res['duration'];
-            $studio = $res['studio_id'];
-
-            $split = explode(":",$stime);
-            $convertedMins = ($split[1] == '30') ? '50' : '00';
-            $simpleStime = ltrim($split[0],'0').$convertedMins;
-            $htmlClasses[] = $studio.'_'.$simpleStime;
-            $duration = $duration*2;
-            for ($i=1;$i<=($duration-.5);$i++) {
-                $timeVerticle = ($simpleStime + 30);
-                //echo substr(($simpleStime+30),1,2);exit;
-                if (substr(($simpleStime+30),1,2) == '30') {
-                    $simpleStime+=70;
-                } elseif (substr($simpleStime+30,2,2) == '30') {
-                    $simpleStime+=70;
-                } else {
-                    $simpleStime+=30;
-                }
-                if ($timeVerticle == '1300') {
-                    $timeVerticle = '100';
-                    $simpleStime = '100';
-                } else if ($timeVerticle == '1370') {
-                    $timeVerticle = '130';
-                    $simpleStime = '130';
-                }
-                $htmlClasses[] = $studio.'_'.$timeVerticle;
-            }
-        }
+        $htmlClasses = DateTimeTools::convertTimeToId($todaysReservations);
 
         $return = array('responseCode' => '200', 'htmlContent' => $htmlClasses);
         $return = json_encode($return);
@@ -77,11 +50,9 @@ class DefaultController extends Controller
         $request = $this->get('request');
 
         $date = $request->request->get('dateclick');
-        $time = $request->request->get('time_container');
+        $time = rtrim($request->request->get('time_container'));
         $time = explode(' ',$time);
-
-        array_pop($time);
-
+//print_r($time);exit;
         $schedularRepository = $this->get('stc_musiczarconia.repository.scheduler');
 
         $reservations = $schedularRepository->findReservationByDate($date);
@@ -94,26 +65,22 @@ class DefaultController extends Controller
 
             $error = '';
             //check database for reservations on given time:
-            foreach ($reservations as $reservation) {
+            $htmlClasses = DateTimeTools::convertTimeToId($reservations);
 
-                $convertedTimes = DateTimeTools::convertTimeToId($reservation);
-                $i = 0;
+            foreach ($time as $timeSlot) {
 
-                foreach ($time as $timeSlot) {
-                    if ($convertedTimes[$i] == $timeSlot) {
-                        $error = "You've selected a time that is already booked for that studio.\nPlease choose
-                                  another time slot.";
-                    }
+                if (in_array($timeSlot,$htmlClasses)) {
+                    $error = "You've selected a time that is already booked for that studio.\nPlease choose
+                              another time slot.";
+                }
 
-                    if ($error !== '') {
-                        $return = array('responseCode' => 200, 'error' => $error);
-                        $return = json_encode($return);
-                        return new Response($return, 200, array('Content-Type' => 'application/json'));
-                    } else {
-                        $i++;
-                    }
+                if ($error !== '') {
+                    $return = array('responseCode' => 200, 'error' => $error);
+                    $return = json_encode($return);
+                    return new Response($return, 200, array('Content-Type' => 'application/json'));
                 }
             }
+
         }
 
         //At this point, there is not conflict in the schedule, proceed to save the appointment:
@@ -138,36 +105,7 @@ class DefaultController extends Controller
         $schedulerRepository = $this->get('stc_musiczarconia.repository.scheduler');
 
         $reservations = $schedulerRepository->findReservationByDate($date);
-        $htmlClasses = array();
-
-        foreach ($reservations as $res) {
-            $stime = $res['reservation_time'];
-            $duration = $res['duration'];
-            $studio = $res['studio_id'];
-
-            $split = explode(":",$stime);
-            $convertedMins = ($split[1] == '30') ? '50' : '00';
-            $simpleStime = ltrim($split[0],'0').$convertedMins;
-            $htmlClasses[] = $studio.'_'.$simpleStime;
-            $duration = $duration*2;
-            for ($i=1;$i<=($duration-.5);$i++) {
-                $timeVerticle = ($simpleStime + 30);
-                //echo substr(($simpleStime+30),1,2);exit;
-                if (substr(($simpleStime+30),1,2) == '30') {
-                    $simpleStime+=70;
-                } elseif (substr($simpleStime+30,2,2) == '30') {
-                    $simpleStime+=70;
-                } else {
-                    $simpleStime+=30;
-                }
-                if ($timeVerticle == '1300') {
-                    $timeVerticle = '100';
-                } else if ($timeVerticle == '1370') {
-                    $timeVerticle = '130';
-                }
-                $htmlClasses[] = $studio.'_'.$timeVerticle;
-            }
-        }
+        $htmlClasses = DateTimeTools::convertTimeToId($reservations);
 
         $return = array('responseCode' => 200, 'htmlContent' => $htmlClasses);
 
--- src/Stc/MusiczarconiaBundle/Tools/DateTimeTools.php
+++ src/Stc/MusiczarconiaBundle/Tools/DateTimeTools.php
@@ -7,36 +7,65 @@ class DateTimeTools
     /*
      * Converts time from database to the id used in the form
      */
-    public static function convertTimeToId($reservation)
+    public static function convertTimeToId($reservations)
     {
         $htmlClasses = array();
 
-        $stime = $reservation['reservation_time'];
-        $duration = $reservation['duration'];
-        $studio = $reservation['studio_id'];
+        $timeArray = [
+            0    => '1200',
+            1    => '1230',
+            2    => '100',
+            3    => '130',
+            4    => '200',
+            5    => '230',
+            6    => '300',
+            7    => '330',
+            8    => '400',
+            9    => '430',
+            10   => '500',
+            11   => '530',
+            12   => '600',
+            13   => '630',
+            14   => '700',
+            15   => '730',
+            16   => '800',
+            17   => '830',
+            18   => '900',
+            19   => '930',
+            20   => '1000',
+            21   => '1030',
+            22   => '1100',
+            23   => '1130'
+        ];
 
-        $split = explode(":",$stime);
-        $convertedMins = ($split[1] == '30') ? '50' : '00';
-        $simpleStime = ltrim($split[0],'0').$convertedMins;
-        $htmlClasses[] = $studio.'_'.$simpleStime;
-        $duration = $duration * 2;
-        for ($i=1;$i<=($duration-.5);$i++) {
-            $timeVerticle = ($simpleStime + 30);
-            //echo substr(($simpleStime+30),1,2);exit;
-            if (substr(($simpleStime+30),1,2) == '30') {
-                $simpleStime+=70;
-            } elseif (substr($simpleStime+30,2,2) == '30') {
-                $simpleStime+=70;
-            } else {
-                $simpleStime+=30;
+        foreach ($reservations as $res) {
+
+            $stime = $res['reservation_time'];
+            $duration = $res['duration'];
+            $studio = $res['studio_id'];
+
+            $split = explode(":",$stime);
+            $hour = ltrim($split[0],"0");
+            $mins = $split[1];
+
+            /*if (substr($hour, 0, 1) == '0') {
+                $hour = substr($hour,1,-1);
+            }*/
+
+            $fullTimeString = $hour . $mins;
+
+            //get the time's position int the timeArray:
+            $timePosition = array_search($fullTimeString, $timeArray);
+            //move up array by key of $duration and retrieve value:
+            for ($i=0; $i<=$duration-1; $i++)
+            {
+                $key = $timePosition + $i;
+                $timeValue = $timeArray[$key];
+                $htmlClasses[] = $studio . "_" . $timeValue;
             }
-            if ($timeVerticle == '1300') {
-                $timeVerticle = '100';
-            } else if ($timeVerticle == '1370') {
-                $timeVerticle = '130';
-            }
-            $htmlClasses[] = $studio.'_'.$timeVerticle;
+
         }
+
         return $htmlClasses;
     }
 }
